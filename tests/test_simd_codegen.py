6cae5418bc4ce6a0f0eb2465ef0e98405f65f2a4
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-04 09:00:23 +0800

    test: define SSE2 int32 relu-add semantics

--- tests/test_simd_codegen.py
+++ tests/test_simd_codegen.py
@@ -8,6 +8,7 @@ from tiny_tensor_compiler import (
     GraphBuilder,
     execute_native,
     execute_reference,
+    fuse_elementwise,
     generate_c,
     lower_to_cpu,
     lower_to_loops,
@@ -26,6 +27,15 @@ def _contiguous_i32_add_program(length: int = 9):
     rhs = builder.input((length,), dtype="int32")
     module = builder.finish(lhs + rhs)
     return module, lower_to_loops(lower_to_cpu(module))
+
+
+def _contiguous_i32_relu_add_program(length: int = 9):
+    builder = GraphBuilder()
+    lhs = builder.input((length,), dtype="int32")
+    rhs = builder.input((length,), dtype="int32")
+    module = builder.finish((lhs + rhs).relu())
+    loops = lower_to_loops(lower_to_cpu(module))
+    return module, fuse_elementwise(loops)
 
 
 def test_generate_c_uses_sse2_for_contiguous_i32_add_with_scalar_tail():
@@ -70,6 +80,68 @@ def test_sse2_i32_add_matches_reference_with_overflow_and_tail():
     actual = execute_native(program, inputs=inputs)
 
     np.testing.assert_array_equal(actual, expected)
+
+
+def test_generate_c_uses_sse2_for_contiguous_i32_relu_add_with_scalar_tail():
+    _, program = _contiguous_i32_relu_add_program()
+
+    source = generate_c(program)
+
+    assert "for (; n + 4 <= 9; n += 4)" in source
+    assert "__m128i sum = _mm_add_epi32(lhs, rhs);" in source
+    assert "__m128i zero = _mm_setzero_si128();" in source
+    assert "__m128i positive = _mm_cmpgt_epi32(sum, zero);" in source
+    assert "__m128i relu = _mm_and_si128(sum, positive);" in source
+    assert "_mm_storeu_si128" in source
+    assert "for (; n < 9; ++n)" in source
+    assert "int32_t value = ((int32_t)p0[n] + (int32_t)p1[n]);" in source
+
+
+def test_sse2_i32_relu_add_matches_reference_after_wrapping_add_and_tail():
+    _default_compiler_or_skip()
+    module, program = _contiguous_i32_relu_add_program()
+    inputs = [
+        np.array(
+            [
+                2_147_483_647,
+                -2_147_483_648,
+                2_000_000_000,
+                -2_000_000_000,
+                5,
+                -5,
+                17,
+                -91,
+                123,
+            ],
+            dtype=np.int32,
+        ),
+        np.array(
+            [1, -1, 2_000_000_000, -2_000_000_000, -10, 10, -17, 91, -456],
+            dtype=np.int32,
+        ),
+    ]
+
+    expected = execute_reference(module, inputs=inputs)
+    actual = execute_native(program, inputs=inputs)
+
+    np.testing.assert_array_equal(actual, expected)
+
+
+def test_sse2_relu_add_path_is_not_used_for_float_or_broadcast_kernels():
+    float_builder = GraphBuilder()
+    float_lhs = float_builder.input((9,), dtype="float32")
+    float_rhs = float_builder.input((9,), dtype="float32")
+    float_module = float_builder.finish((float_lhs + float_rhs).relu())
+    float_program = fuse_elementwise(lower_to_loops(lower_to_cpu(float_module)))
+
+    broadcast_builder = GraphBuilder()
+    broadcast_lhs = broadcast_builder.input((2, 1), dtype="int32")
+    broadcast_rhs = broadcast_builder.input((1, 3), dtype="int32")
+    broadcast_module = broadcast_builder.finish((broadcast_lhs + broadcast_rhs).relu())
+    broadcast_program = fuse_elementwise(lower_to_loops(lower_to_cpu(broadcast_module)))
+
+    assert "_mm_cmpgt_epi32" not in generate_c(float_program)
+    assert "_mm_cmpgt_epi32" not in generate_c(broadcast_program)
 
 
 def test_sse2_path_is_not_used_for_float_or_broadcast_add():
