37d08dfec7d71696a0a0d1284cc05d0482406afd
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-06 02:02:51 +0800

    fix: correct singleton contiguity fixture

--- tests/test_layout_contiguity.py
+++ tests/test_layout_contiguity.py
@@ -4,7 +4,7 @@ from tiny_tensor_compiler.layout import StorageLayout
 def test_singleton_axis_stride_does_not_break_c_contiguity():
     assert StorageLayout(offset=0, strides=(-1, 1)).is_contiguous((1, 1))
     assert StorageLayout(offset=0, strides=(-7, 1)).is_contiguous((1, 4))
-    assert StorageLayout(offset=0, strides=(4, -9)).is_contiguous((3, 1))
+    assert StorageLayout(offset=0, strides=(1, -9)).is_contiguous((3, 1))
 
 
 def test_empty_layout_has_no_element_order_to_break_contiguity():
