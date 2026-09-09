f4e6aa6f8c9ebbf1a89ed04fe21a26a3e3ed8f39
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-09 18:28:12 +0800

    style: format effect scheduler test imports

--- tests/test_effect_schedule.py
+++ tests/test_effect_schedule.py
@@ -4,7 +4,13 @@ import shutil
 import numpy as np
 import pytest
 
-from tiny_tensor_compiler import GraphBuilder, compile_module, generate_c, lower_to_cpu, lower_to_loops
+from tiny_tensor_compiler import (
+    GraphBuilder,
+    compile_module,
+    generate_c,
+    lower_to_cpu,
+    lower_to_loops,
+)
 from tiny_tensor_compiler.effect_schedule import plan_parallel_effect_groups
 from tiny_tensor_compiler.loop_ir import LoopCopyInto, LoopInplaceBinary
 
